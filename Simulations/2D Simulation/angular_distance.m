ee81774aee7db44c3e41874b899a81c862154afa
WentaoWang <wentaowang1999@outlook.com>
2022-09-17 23:08:47 +0800

    delete

--- Simulations/2D Simulation/angular_distance.m
+++
@@ -1,9 +0,0 @@
-function [theta] = angular_distance(Re,R)
-%Input
-%Re(2,2)            : estimated rotation matrix
-%R(2,2)             : true rotation matrix
-%Output
-%theta              : angular distance between estimated and true rotation
-R0=R*Re';
-theta=acosd(R0(1,1));
-end
